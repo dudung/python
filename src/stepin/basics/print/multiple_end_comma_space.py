972cde8fcb835d1a5d746a1779b88ae8338bbb92
dudung <dudung@gmail.com>
2023-03-11 19:15:47 +0700

    new 5, pause isya

---
+++ src/stepin/basics/print/multiple_end_comma_space.py
@@ -0,0 +1,10 @@
+print("1st line", end=', ')
+print("still 1st line", end=', ')
+print("again still 1st line", end=', ')
+print("and of course still 1st line")
+
+
+"""
+1st line, still 1st line, again still 1st line, and of course still 1st line
+PS L:\home\python>
+"""
