ad0d427f4db826c29636181fc33141e3caa9807b
dudung <dudung@gmail.com>
2023-03-12 11:37:26 +0700

    update

--- src/stepin/basics/comment/comment_out.py
+++ src/stepin/basics/comment/comment_out.py
@@ -1,11 +1,15 @@
-"""
-multi_line.py
-define x
-diplay its value
-"""
-x = 0
+#x = 0 -- change code but reserve the old one
+x = 1
 print(x)
 
+""" -- change lines of code but reserve the old ones
+y = 2
+print(y)
 """
-0
+y = x + 2
+print(y)
+
+"""
+1
+3
 """
