1e2f924bab636dce26292e0934f3345e5007d7ef
Sparisoma Viridi <dudung@gmail.com>
2023-03-12 17:56:47 +0700

    Update range.py

--- src/stepin/basics/for/range.py
+++ src/stepin/basics/for/range.py
@@ -1,9 +1,14 @@
 for i in range(2, 10, 1):
-  print(i)
+  print(i, end=' ')
+print()
 
 x = range(4, 9)
 for i in x:
-  print(i)
+  print(i, end=' ')
+print()
+
 
 """
+2 3 4 5 6 7 8 9 
+4 5 6 7 8
 """
