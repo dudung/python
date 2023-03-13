2613e0ca192ddfd0a18eec8af8aa77d1b41bebfc
Sparisoma Viridi <dudung@gmail.com>
2023-03-13 07:38:27 +0700

    function +1

---
+++ src/stepin/basics/function/add.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    a = x + y
+    return a
+
+x = 2.5
+y = 1.2
+z = add(x, y)
+print("x =", x)
+print("y =", y)
+print("z = x + y =", z)
+
+
+"""
+x = 2.5
+y = 1.2
+z = x + y = 3.7
+"""
