fa48f0320bc7905a4916a595b36d40193f439eb9
dudung <dudung@gmail.com>
2023-03-15 05:24:36 +0700

    new, not complete, pause

---
+++ src/stepin/basics/function/sumproduct.py
@@ -0,0 +1,19 @@
+def sum(*args):
+  s = 0
+  for i in args:
+    s += i
+  return s
+
+def prod(*args):
+  p = 1
+  for i in args:
+    p *= i
+  return p
+
+def sumprod(*args):
+  sp = 0
+  for i in args:
+    pass
+
+"""
+"""
