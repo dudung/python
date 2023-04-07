a6cd4c24245afa9254a9be2cbee7b263d51f7297
dudung <dudung@gmail.com>
2023-04-07 18:40:15 +0700

    update

--- src/import/inhouse/id-data/tests/ut_cimahi.py
+++ src/import/inhouse/id-data/tests/ut_cimahi.py
@@ -25,3 +25,26 @@ print()
 print("Cimahi Selatan PDRB ADHK sector C in 2017")
 print(cims.pdrb_adhk['C']['2017'])
 print()
+
+
+"""
+$ python -m tests.ut_cimahi
+Cimahi Selatan population in 2021
+240990000
+
+Cimahi Tengah population in 2021
+161758000
+
+Cimahi Utara population in 2021
+165652000
+
+Cimahi Selatan PDRB ADHK sector A in 2016
+32366200000.0
+
+Cimahi Selatan PDRB ADHK sector C in 2016
+8626902500.0
+
+Cimahi Selatan PDRB ADHK sector C in 2017
+8983519100.0
+
+"""
--- src/import/inhouse/id-data/tests/ut_lembang.py
+++ src/import/inhouse/id-data/tests/ut_lembang.py
@@ -1,3 +1,9 @@
 import id_data.jawabarat.bandungbarat.lembang as lem
 
 print(lem.name, lem.level, lem.area)
+
+
+"""
+$ python -m tests.ut_lembang
+Lembang 3 95.56
+"""
