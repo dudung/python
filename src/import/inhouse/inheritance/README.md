1100183ec7121b5c5ba90c14c9e355e74f1b83b6
dudung <dudung@gmail.com>
2023-04-08 04:29:17 +0700

    update readmes

--- src/import/inhouse/inheritance/README.md
+++ src/import/inhouse/inheritance/README.md
@@ -2,4 +2,5 @@
 use inhouse modules to learn inheritance
 
 + [device](device.py)
-+ [test_device](test_device.py)
++ [connect_device](connect_device.py)
++ [scan_devices](scan_devices.py)
--- src/import/inhouse/inheritance/device.py
+++ src/import/inhouse/inheritance/device.py
@@ -6,6 +6,8 @@ class Computer:
   
   def add(self, dev):
     self.devices.append(dev)
+    dev.status = "connected"
+    dev.host = self.name
   
   def list(self):
     lines = ""
@@ -20,48 +22,64 @@ class Monitor:
   def __init__(self, name="Monitor"):
     self.name = name
     self.type = "output"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Keyboard:
   def __init__(self, name="Keyboard"):
     self.name = name
     self.type = "input"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Mouse:
   def __init__(self, name="Mouse"):
     self.name = name
     self.type = "input"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Printer:
   def __init__(self, name="Printer"):
     self.name = name
     self.type = "output"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Scanner:
   def __init__(self, name="Scanner"):
     self.name = name
     self.type = "input"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Network:
   def __init__(self, name="Network"):
     self.name = name
     self.type = "bidirectional"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Speaker:
   def __init__(self, name="Speaker"):
     self.name = name
     self.type = "output"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Microphone:
   def __init__(self, name="Microphone"):
     self.name = name
     self.type = "input"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Camera:
@@ -69,18 +87,24 @@ class Camera:
     self.name = name
     self.type = "input"
     self.zoom = "digital"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Digitizer:
   def __init__(self, name="Digitizer"):
     self.name = name
     self.type = "input"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Storage:
   def __init__(self, name="Storage"):
     self.name = name
     self.type = "bidirectional"
+    self.status = "disconnected"
+    self.host = "none"
 
 
 class Microscope(Camera):
