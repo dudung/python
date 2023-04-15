f0be92365f33bb2fb11bcfa8f8ca81277c779887
dudung <dudung@gmail.com>
2023-04-16 04:34:45 +0700

    update readmes

---
+++ src/stepin/intermediate/list_of_list/plus_op_list_l1.py
@@ -0,0 +1,14 @@
+x = [1, 2]
+y = [3, 4]
+z = x + y
+
+print("x =", x)
+print("y =", y)
+print("z = x + y =", z)
+
+
+"""
+$ python plus_op_list_l1.py  x = [1, 2]
+y = [3, 4]
+z = x + y = [1, 2, 3, 4]
+"""
