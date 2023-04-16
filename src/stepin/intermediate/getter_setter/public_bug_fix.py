390dbc2485eaea7f3f29b8f2f91baa1ba111d00d
dudung <dudung@gmail.com>
2023-04-16 13:52:55 +0700

    update, new

---
+++ src/stepin/intermediate/getter_setter/public_bug_fix.py
@@ -0,0 +1,39 @@
+import math
+
+class Thing:
+  
+  def __init__(self, mass=1, volume=1):
+    self.mass = mass
+    self.volume = volume
+    
+  def __str__(self):
+    value = ""
+    value += f"mass = {self.mass}\n"
+    value += f"volume = {self.volume}\n"
+    value += f"density = {self.density()}"
+    return value
+  
+  def density(self):
+    value = self.mass / self.volume
+    return value
+
+z = Thing(2.5, 0.5)
+print(z)
+
+print()
+
+z.mass = 2
+z.volume = 4
+print(z)
+
+
+"""
+$ python public_bug.py
+mass = 2.5
+volume = 0.5
+density = 5.0
+
+mass = 2
+volume = 4
+density = 0.5
+"""
