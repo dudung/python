970cf29cbc18efa9a8e10c886fae339a0bd41213
dudung <dudung@gmail.com>
2023-04-16 20:12:26 +0700

    llist +2

---
+++ src/stepin/intermediate/linked_list_v2/linkedlist_pop.py
@@ -0,0 +1,17 @@
+from linkedlist import LinkedList
+
+ll = LinkedList()
+ll.append(100)
+ll.append("Hello")
+ll.append(True)
+ll.append(98.34E-4)
+
+print(ll.count)
+print(ll.list())
+
+
+"""
+$ python linkedlist_instance.py
+4
+[100, 'Hello', True, 0.009834]
+"""
