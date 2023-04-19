6696c1139ba3006ee33ffa193a0040aef9a3e994
dudung <dudung@gmail.com>
2023-04-19 12:33:49 +0700

    fin

--- src/stepin/intermediate/ann/layers_2321_feedforward.py
+++ src/stepin/intermediate/ann/layers_2321_feedforward.py
@@ -7,33 +7,75 @@ sys.path.append("../")
 
 import matrix.matrix as mtx
 
-X = [
+X1 = [
   [0, 0, 1, 1],
   [0, 1, 0, 1]
 ]
 
-W = [
-  [0.845, 0.845]
+Y = [
+  [0],
+  [1],
+  [0],
+  [1]
 ]
 
-Z = mtx.mul(W, X)
-Y = mtx.map(Z, sigmoid)
+W21 = [
+  [1, 1],
+  [1, 1],
+  [1, 1]
+]
 
-[print(row) for row in X]
+W32 = [
+  [1, 1, 1],
+  [1, 1, 1]
+]
+
+W43 = [
+  [1, 1]
+]
+
+X2 = mtx.map(mtx.mul(W21, X1), sigmoid)
+X3 = mtx.map(mtx.mul(W32, X2), sigmoid)
+X4 = mtx.map(mtx.mul(W43, X3), sigmoid)
+
+print("X1")
+[print(row) for row in X1]
 print()
 
-[print(row) for row in W]
+print("X2")
+[print(row) for row in X2]
 print()
 
-[print([f"{i:.1f}" for i in row]) for row in Y]
+print("X3")
+[print(row) for row in X3]
+print()
+
+print("X4")
+[print(row) for row in X4]
+print()
+
+print("Y")
+[print(row) for row in mtx.tpose(Y)]
 
 
 """
-$ python layers_21_matrix_and.py
+$ python layers_2321_feedforward.py
+X1
 [0, 0, 1, 1]
 [0, 1, 0, 1]
 
-[0.845, 0.845]
+X2
+[0.50, 0.73, 0.73, 0.88]
+[0.50, 0.73, 0.73, 0.88]
+[0.50, 0.73, 0.73, 0.88]
 
-['0.5', '0.7', '0.7', '0.8']
+X3
+[0.82, 0.90, 0.90, 0.93]
+[0.82, 0.90, 0.90, 0.93]
+
+X4
+[0.84, 0.86, 0.86, 0.87]
+
+Y
+[0, 1, 0, 1]
 """
