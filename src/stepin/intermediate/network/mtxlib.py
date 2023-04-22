191b9407f8875732786fec9b796ac1c7bbb55552
dudung <dudung@gmail.com>
2023-04-23 05:29:32 +0700

    remove blank lines

--- src/stepin/intermediate/network/mtxlib.py
+++ src/stepin/intermediate/network/mtxlib.py
@@ -1,3 +1,7 @@
+import copy
+import random
+import math
+
 # create zero matrix with rnum rows and cnum columns
 def zero(rnum, cnum):
   m = []
@@ -8,7 +12,6 @@ def zero(rnum, cnum):
       r.append(c)
     m.append(r)
   return m
-
 
 # get string representation of matrix
 def mat2str(m, fmt=".2e"):
@@ -25,12 +28,10 @@ def mat2str(m, fmt=".2e"):
     mstr += rstr
   return mstr
 
-
 # stack rows of two matrices -- to-do unmacth dimension
 def stackrows(x, y):
   z = x + y
   return z
-
 
 # stack columns of two matrices -- to-do unmacth dimension
 def stackcols(x, y):
@@ -42,7 +43,6 @@ def stackcols(x, y):
       z[r].append(y[r][c])
   return z
 
-
 # addition of two matrices -- to-do unmacth dimension
 def add(x, y):
   rnum = min(len(x), len(y))
@@ -53,7 +53,6 @@ def add(x, y):
       z[i][j] = x[i][j] + y[i][j]
   return z
 
-
 # substraction of two matrices -- to-do unmacth dimension
 def sub(x, y):
   rnum = min(len(x), len(y))
@@ -63,7 +62,6 @@ def sub(x, y):
     for j in range(cnum):
       z[i][j] = x[i][j] - y[i][j]
   return z
-
 
 # multiplication of two matrices -- to-do unmacth dimension
 def mul(x, y):
@@ -79,7 +77,6 @@ def mul(x, y):
       z[i][j] = s
   return z
 
-
 # map input matrix to output one via a function
 def map(x, f):
   rnum = len(x)
@@ -89,7 +86,6 @@ def map(x, f):
     for c in range(cnum):
       y[r][c] = f(x[r][c])
   return y
-
 
 # transpose a matrix
 def tpose(x):
@@ -101,7 +97,6 @@ def tpose(x):
       y[c][r] = x[r][c]
   return y
 
-
 # trace of a square matrix
 def trace(x):
   num = len(x)
@@ -110,19 +105,13 @@ def trace(x):
     y += x[i][i]
   return y
 
-
 # get number of rows and colums of a matrix
 def getinfo(m):
   rnum = len(m)
   cnum = len(m[0])
   return rnum, cnum
 
-##
-
-import random
-import math
-import copy
-
+# randomize a matrix, initial matrix is changed
 def randomize(m):
   rnum = len(m)
   cnum = len(m[0])
@@ -130,6 +119,7 @@ def randomize(m):
     for c in range(cnum):
       m[r][c] = 0.01 * random.randint(-100, 100)
 
+# randomize a matrix, return the result as new matrix
 def randomized(n):
   m = copy.deepcopy(n)
   rnum = len(m)
@@ -139,6 +129,7 @@ def randomized(n):
       m[r][c] = 0.01 * random.randint(-100, 100)
   return m
 
+# round matrix element to float with some digits
 def roundmat(m, ndigits):
   rnum = len(m)
   cnum = len(m[0])
@@ -146,16 +137,19 @@ def roundmat(m, ndigits):
     for c in range(cnum):
       m[r][c] = round(m[r][c], ndigits)
 
+# obtain column c of a matrix
 def columnof(c, m):
   value = []
   for r in m:
     value.append(r[c])
   return value
 
+# obtain row r of a matrix
 def rowof(r, m):
   value = m[r]
   return value
 
+# square difference of two matrices
 def sqrdiff(x, y): # to-do mismatch dimension of x and y matrices
   rnum = min(len(x), len(y))
   cnum = min(len(x[0]), len(y[0]))
@@ -169,10 +163,7 @@ def sqrdiff(x, y): # to-do mismatch dimension of x and y matrices
     m.append(row)
   return m
 
-def mulscalar(z):
-  eta = 0.001
-  return -eta * z
-
+# gradient of value for change of matrix elements
 def grad(dval, dm):
   rnum = len(dm)
   cnum = len(dm[0])
