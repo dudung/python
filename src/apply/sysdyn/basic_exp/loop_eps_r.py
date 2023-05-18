c97a09bee3e8b56aa1c509eed867a24add99c974
dudung <dudung@gmail.com>
2023-05-18 16:59:46 +0700

    solved simples loop

--- src/apply/sysdyn/basic_exp/loop_eps_r.py
+++ src/apply/sysdyn/basic_exp/loop_eps_r.py
@@ -1,17 +1,26 @@
-R = 2.5
-eps = 10
-I = 2
-V = 5
+eps = 16
+R = 4
+
+I = -12.34
+V = 500.005
+
 print("eps =", eps)
 print("R =", R)
 print()
 
+Vnew = V + 100
 print("t\tI\tV")
-for t in range(10):
+for t in range(5):
   print(t, I, V, sep='\t')
   
-  dI = (eps - V) / R
-  dV = R * dI
-  
-  V = V + dV
+  dI =  -(1/R) * V + (eps/R)
   I = I + dI
+  
+  V = R * I
+  
+  if abs(Vnew - V) < 1E-5:
+    print()
+    print("Solved in", t, "iteration")
+    break
+  else:
+    Vnew = V
