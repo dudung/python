9e11ad27f20ce901bfdb99fc6a99f1b8719571a7
dudung <dudung@gmail.com>
2023-06-03 02:33:15 +0700

    new eye+2

---
+++ src/import/external/numpy/matrix/eye_k_plus_1.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+m = np.eye(5, k=1)
+
+print("m =")
+print(m)
+
+print()
+print("ndim =", m.ndim)
+print("shape =", m.shape)
+
+
+"""
+$ python eye_k_plus_1.py
+m =
+[[0. 1. 0. 0. 0.]
+ [0. 0. 1. 0. 0.]
+ [0. 0. 0. 1. 0.]
+ [0. 0. 0. 0. 1.]
+ [0. 0. 0. 0. 0.]]
+
+ndim = 2
+shape = (5, 5)
+"""
