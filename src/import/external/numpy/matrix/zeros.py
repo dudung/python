fd273a543a22e808aed2a737e19e0cd04e621226
dudung <dudung@gmail.com>
2023-06-03 02:20:24 +0700

    new zeros

---
+++ src/import/external/numpy/matrix/zeros.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+m = np.zeros([5, 3])
+
+print("m =")
+print(m)
+
+print()
+print("ndim =", m.ndim)
+print("shape =", m.shape)
+
+
+"""
+$ python zeros.py
+m =
+[[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+
+ndim = 2
+shape = (5, 3)
+"""
