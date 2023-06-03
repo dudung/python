aed3d014a709396bb9781833f241d311d1adbe54
dudung <dudung@gmail.com>
2023-06-03 11:43:35 +0700

    matrix multiplication

---
+++ src/import/external/numpy/operation/multiplication.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+a = np.array(
+  [
+    [1, 2, 3],
+    [0, 1, 2]
+  ]
+)
+
+b = np.array(
+  [
+    [1, 0],
+    [1, 1],
+    [3, 2]
+  ]
+)
+
+c = a @ b
+
+print("a =")
+print(a)
+print()
+
+print("b =")
+print(b)
+print()
+
+print("c =")
+print(c)
+
+
+"""
+"""
