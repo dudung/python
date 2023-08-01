898d10d38096e7af4745f25aabaac3ad0e45ac10
dudung <dudung@gmail.com>
2023-08-01 20:35:35 +0700

    update

--- src/apply/rectgrid/defndisp/defmatconsole.py
+++ src/apply/rectgrid/defndisp/defmatconsole.py
@@ -1,15 +1,33 @@
+# defmatconsole.py
 # define matrix and print to console
+# Sparisoma Viridi | https://github.com/dudung
+# 20230801
 
-def zero(rows, cols) {
+def zero(rows, cols):
   m = []
   for i in range(rows):
     r = []
     for j in range(cols):
       c = 0
       r.append(c)
-    m.append(c)
+    m.append(r)
   return m
-}
 
-m1 = zero(5, 10)
-print(m1)
+def mat2lines(m):
+  s = ""
+  for i in m:
+    s += f"{i}" + "\n"
+  return s
+
+m1 = zero(5, 3)
+print(mat2lines(m1))
+
+
+"""
+$ python defmatconsole.py
+[0, 0, 0]
+[0, 0, 0]
+[0, 0, 0]
+[0, 0, 0]
+[0, 0, 0]
+"""
